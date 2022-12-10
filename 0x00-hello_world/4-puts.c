16de0fbc2840da313d69538fda9c7e3d38719d33
scodeq65 <sodiqagbaraojo@gmail.com>
2022-12-10 00:50:00 -0800

    correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>                                                       
 /**
- *  * main - Prints some lines of code without using printf but using put   
+ * main - Prints some lines of code without using printf but using put   
  *
  *Description: 'using puts instead of printf'
  * Return: 0    
  */
 int main(void)
 {
-         puts("\"Programming is like building a multilingual puzzle");    
-	 return (0);
+	puts("\"Programming is like building a multilingual puzzle");    
+       	return (0);
 }
