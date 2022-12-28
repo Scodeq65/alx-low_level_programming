1086f2467cba503470529cc0f088667720046950
scodeq65 <sodiqagbaraojo@gmail.com>
2022-12-27 19:38:23 -0800

    extra

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 		_putchar(0);
 		for (column = 1; column <= 9; column++)
 		{
-			_putchar(',');
+			_putchar('0');
 			_putchar(' ');
 			product = row * column;
 			if (product <= 9)
