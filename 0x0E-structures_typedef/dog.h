427c9e6476db6f70e63b038c7d5c598af7a63569
scodeq65 <sodiqagbaraojo@gmail.com>
2023-11-07 02:51:30 -0800

    minor correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,19 +9,12 @@
  * Description: my first struct with Alx.
  */
 
-
-/**
- * dog_t - new anme for struct dog
- */
-
-typedef struct dog dog_t;
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
