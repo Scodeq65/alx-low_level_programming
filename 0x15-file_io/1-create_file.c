2c520e143ea9714ca5bf1f4fab9f1b019e15a67e
scodeq65 <sodiqagbaraojo@gmail.com>
2023-12-03 19:13:32 -0800

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fld = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	fld = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fld == -1)
 		return (-1);
