2efdbdc122720f975ef687c74ee542d5944df1cb
scodeq65 <sodiqagbaraojo@gmail.com>
2024-03-24 02:27:49 -0700

    task3_updated

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <string.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /* MAIN_H */
